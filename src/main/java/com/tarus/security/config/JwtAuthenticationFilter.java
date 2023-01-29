bb6bbd612b646e9aad3e6032f38ca627bd86e89e
Victor Tarus <tarus@Victors-MacBook-Pro.local>
2023-01-29 22:27:28 +0200

    Implemented authentication using jwt

---
+++ src/main/java/com/tarus/security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,58 @@
+package com.tarus.security.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor //creates constructor using any final field we declare
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request, //our request
+           @NonNull HttpServletResponse response, // our response we can add header
+           @NonNull FilterChain filterChain //contains list of other filters we need to add
+    ) throws ServletException, IOException {
+final String authHeader = request.getHeader("Authorization");
+final String jwt;
+final  String userEmail;
+if(authHeader == null || !authHeader.startsWith("Bearer ")){
+    filterChain.doFilter(request,response);
+    return;
+}
+jwt = authHeader.substring(7);
+userEmail = jwtService.extractUsername(jwt);
+
+/**@desc check if user is authenticated*/
+if(userEmail != null && SecurityContextHolder.getContext().getAuthentication()== null){
+    UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+    if(jwtService.isTokenValid(jwt,userDetails)){
+        UsernamePasswordAuthenticationToken  authenticationToken = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                null,
+                userDetails.getAuthorities()
+        );
+        authenticationToken.setDetails(
+                new WebAuthenticationDetailsSource().buildDetails(request)
+        );
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+    }
+    filterChain.doFilter(request,response);
+}
+
+    }
+}
---
+++ src/main/java/com/tarus/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.tarus.security.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
